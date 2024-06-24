afd03ef1904f9d221c7355ad45109ff85974cb08
Zuhanit <zuhanit3@gmail.com>
2024-06-24 17:38:39 +0900

    feat: module setting

---
+++ src/tileset/__init__.py
@@ -0,0 +1,29 @@
+from .terrain import (
+    read,
+    Tilesets,
+    CV5,
+    CV5Doodad,
+    CV5Tile,
+    VF4,
+    VF4Flag,
+    VX4,
+    VX4Type,
+    VR4,
+    WPE,
+    WPEType,
+)
+
+__all__ = [
+    "read",
+    "Tilesets",
+    "CV5",
+    "CV5Doodad",
+    "CV5Tile",
+    "VF4",
+    "VF4Flag",
+    "VX4",
+    "VX4Type",
+    "VR4",
+    "WPE",
+    "WPEType",
+]
